692fda6d28138b38ec8cf5a386a2e83ce8722d37
Dobrin Ivanov <dobsc2@gmail.com>
2022-11-21 20:25:05 +0200

    implement user&profile; create&configure shop app

--- friendoftech/accounts/managers.py
+++ friendoftech/accounts/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.hashers import make_password
 
 
-class MyUserManager(BaseUserManager):
+class AppUserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
